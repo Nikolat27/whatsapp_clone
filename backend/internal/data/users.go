09fd4d5a7ec9abbc2c51a63bb10971d3fe9d8273
nikolat27 <samalizadeh899@gmail.com>
2025-06-28 16:23:35 +0330

    Added user delete

--- backend/internal/data/users.go
+++ backend/internal/data/users.go
@@ -38,6 +38,7 @@ func (u *UserModel) CreateUserInstance(username, password string) error {
 		Password:  hashedPassword,
 		CreatedAt: time.Now().UTC(),
 	}
+
 	_, err = u.DB.Collection(userCollection).InsertOne(ctx, user)
 	return err
 }
@@ -46,7 +47,9 @@ func (u *UserModel) GetUserInstance(username string) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	filter := bson.D{{"username", username}}
+	filter := bson.M{
+		"username": username,
+	}
 
 	var user User
 	err := u.DB.Collection(userCollection).FindOne(ctx, filter).Decode(&user)
@@ -57,4 +60,24 @@ func (u *UserModel) GetUserInstance(username string) (*User, error) {
 		return nil, fmt.Errorf("failed to find user '%s': %w", username, err)
 	}
 	return &user, nil
+}
+
+func (u *UserModel) DeleteUserInstance(userId primitive.ObjectID) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	filter := bson.M{
+		"_id": userId,
+	}
+	
+	result,  err := u.DB.Collection(userCollection).DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	if result.DeletedCount == 0 {
+		return errors.New("no user found to delete. 404")
+	}
+
+	return nil
 }
